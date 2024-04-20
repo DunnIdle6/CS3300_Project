25b5a95bc776d87a8f6e235014ef338ea35e67c2
Dunn Idle <didlevi@uccs.edu>
2024-04-20 12:19:14 -0600

    one test

--- ensembled_app/tests.py
+++ ensembled_app/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import * #import all the models for testing
+
+#2test models, 2test forms, 2test views
 
 # Create your tests here.
+class TestBand(TestCase):
+    def setUp(self):
+        Band.objects.create(name="band1", genre="Jazz", about="ABOUT ME BAND", isOpen="False")
+
+    def testBandSTR(self):
+        band = Band.objects.get(name="band1")
+        self.assertEqual(band.__str__(), "band1")
+
+    def testBandName(self):
+        band = Band.objects.get(name="band1")
+        self.assertEqual(band.name, "band1")    
