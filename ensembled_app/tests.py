5bfc1b60406a551cdd1d72705e65a2a0eec1eef7
Dunn Idle <didlevi@uccs.edu>
2024-04-26 17:59:54 -0600

    some tests

--- ensembled_app/tests.py
+++ ensembled_app/tests.py
@@ -1,17 +1,88 @@
 from django.test import TestCase
 from .models import * #import all the models for testing
+from .forms import *
 
 #2test models, 2test forms, 2test views
+#checkout https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Testing
 
 # Create your tests here.
-class TestBand(TestCase):
-    def setUp(self):
-        Band.objects.create(name="band1", genre="Jazz", about="ABOUT ME BAND", isOpen="False")
+class MusicianModelTest(TestCase):
+    def setUpTestData():
+        Musician.objects.create(name = 'mr. test', instruments = 'instrument')
+    
+    def test_name_label(self):
+        musician = Musician.objects.get(id=1)
+        field_label = musician._meta.get_field('name').verbose_name
+        self.assertEqual(field_label, 'name')
 
-    def testBandSTR(self):
-        band = Band.objects.get(name="band1")
-        self.assertEqual(band.__str__(), "band1")
+    def test_instruments_label(self):
+        musician = Musician.objects.get(id=1)
+        field_label = musician._meta.get_field('instruments').verbose_name
+        self.assertEquals(field_label, 'instruments')
 
-    def testBandName(self):
-        band = Band.objects.get(name="band1")
-        self.assertEqual(band.name, "band1")    
+    def test_isLooking_default(self):
+        musician = Musician.objects.get(id=1)
+        self.assertTrue(musician.isLooking)
+
+    def test_get_absolute_url(self):
+        musician = Musician.objects.get(id=1)
+        self.assertEquals(musician.get_absolute_url(), '/musicians/1')
+
+class BandModelTest(TestCase):
+    def setUpTestData():
+        Band.objects.create(name='TestBand', genre='Rock')
+
+    def test_name_label(self):
+        band = Band.objects.get(id=1)
+        field_label = band._meta.get_field('name').verbose_name
+        self.assertEquals(field_label, 'name')
+
+    def test_genre_label(self):
+        band = Band.objects.get(id=1)
+        field_label = band._meta.get_field('genre').verbose_name
+        self.assertEquals(field_label, 'genre')
+
+    def test_isOpen_default(self):
+        band = Band.objects.get(id=1)
+        self.assertTrue(band.isOpen)
+
+    def test_members_relation(self):
+        musician = Musician.objects.create(name='mr. test', instruments='instrument')
+        band = Band.objects.get(id=1)
+        band.members.add(musician)
+        self.assertIn(musician, band.members.all())
+
+    def test_get_absolute_url(self):
+        band = Band.objects.get(id=1)
+        self.assertEquals(band.get_absolute_url(), '/bands/1')
+
+class MusicianFormTest(TestCase):
+    def test_musician_form_valid_data(self):
+        form = MusicianForm(data={
+            'name': 'a name',
+            'email': 'an email',
+            'about': 'a brief sentence',
+            'instruments': 'instrument',
+            'isLooking': True
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_musician_form_blank_data(self):
+        form = MusicianForm(data={})
+        self.assertFalse(form.is_valid())
+
+class BandFormTest(TestCase):
+    def test_band_form_valid_data(self):
+        form = BandForm(data={
+            'name': 'band name',
+            'genre': 'Rock',
+            'about': 'sentencey.',
+            'isOpen': True,
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_band_form_blank_data(self):
+        form = BandForm(data={})
+        self.assertFalse(form.is_valid())
+
+        
